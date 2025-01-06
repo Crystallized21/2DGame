026cf895176e2eb7a94bb6aa3f5a2209c94891c4
Michael Bui <michael.bui_crystal@outlook.com>
2025-01-07 00:07:39 +1300

    todo: for later use

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,6 +12,12 @@ public class Main {
     public static void main(String[] args) {
         // Initialize the graphics context
         // TODO: Tabbing out of the game causes the game to have high FPS
+
+        // TODO: Feedback from play testing
+        // Not enough coins being dropped
+        // Entity Boss invisible glitch
+        
+
         initializeGraphics();
 
         window = new JFrame();
