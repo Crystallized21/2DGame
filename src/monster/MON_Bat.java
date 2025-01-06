93213f0c284951b96008a2d359772ed3eba8a7d7
Michael Bui <michael.bui_crystal@outlook.com>
2025-01-06 23:46:01 +1300

    Rename the name cause i forgot 💀

--- src/monster/MON_Bat.java
+++ src/monster/MON_Bat.java
@@ -17,7 +17,7 @@ public class MON_Bat extends Entity {
         this.gp = gp;
 
         type = type_monster;
-        name = "Green Slime";
+        name = "Bat";
         defaultSpeed = 4;
         speed = defaultSpeed;
         maxLife = 7;
