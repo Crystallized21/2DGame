13ceb2f81dc4e539872b5a25968dd8fb6d121ff6
Michael Bui <michael.bui_crystal@outlook.com>
2025-01-07 17:11:51 +1300

    projectile calc fix and balence

--- src/entity/Projectile.java
+++ src/entity/Projectile.java
@@ -28,7 +28,7 @@ public class Projectile extends Entity {
             int monsterIndex = gp.cChecker.checkEntity(this, gp.monster);
 
             if (monsterIndex != 999) {
-                gp.player.damageMonster(monsterIndex, this, attack * (gp.player.level / 2), knockBackPower);
+                gp.player.damageMonster(monsterIndex, this, Math.max(1, attack * (gp.player.level / 2)), knockBackPower);
                 generateParticle(user.projectile, gp.monster[gp.currentMap][monsterIndex]);
                 alive = false;
             }
